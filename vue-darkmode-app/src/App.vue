10e470f14786ff0067bb2382995b63e80f20292a
aangelicap <angelicapelizza@hotmail.com>
2023-03-10 10:14:14 -0300

    Create structure for dark mode using vue and vite

---
+++ vue-darkmode-app/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark({
+  selector: "body", //element to add attribute to
+  attribute: "theme", // attribute name
+  valueDark: "custom-dark", // attribute value for dark mode
+  valueLight: "custom-light", // attribute value for light mode
+});
+
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  <p>Dark theme: {{isDark}}</p>
+
+  <button @click="toggleDark()">
+    Toggle Color Mode
+  </button>
+</template>
+
+<style>
+[theme="custom-dark"] {
+  background: #3a3b42;
+  color: #fff;
+}
+
+[theme="custom-light"] {
+  background: #ecb7ea;
+  color: #000000;
+}
+</style>
